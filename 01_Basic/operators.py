a5d008c1ff2b7354d3792eb1666579514106e6c3
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-04-16 09:11:10 +0530

    feat:operators in python.

---
+++ 01_Basic/operators.py
@@ -0,0 +1,29 @@
+# age = int(input("Enter your age : "))
+# is_license = False
+
+# can_drive = age>=18 and is_license  
+# if can_drive:
+#     print("You can drive!")
+# else:
+#     print("You can't drive!")
+
+# Code which i tried to run.
+age = int(input("Enter your age : "))
+while True:
+    is_license = input("Do you have driving license (yes/no) : ")
+    if is_license.lower() == "yes":
+        is_license = True
+        break
+    elif is_license.lower() == "no":
+        is_license = False
+        break
+    else:
+        print("Please enter a valid choice (yes/no)-")
+
+can_drive = age>=18 and is_license
+
+can_drive = age>=18 and is_license  
+if can_drive:
+    print("You can drive!")
+else:
+    print("You can't drive!")
