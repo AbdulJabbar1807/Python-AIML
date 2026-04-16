03f2d83a8140de97717ea5dcc8fcc0e392a6eb48
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-04-16 09:09:53 +0530

    fix:minor fixes.

--- 01_Basic/variables.py
+++ 01_Basic/variables.py
@@ -1,9 +1,10 @@
-age = 21 #interger
+age = 21 #int Datatype.
 print(f"My age is {age}")
 
-a = 2.78 #float
-b = 6.87 #float
+a = 2.78 #float Datatype.
+b = 6.87 #float Datatype.
 print(f"Sum of a,b is {a+b}")
 
-is_student = True
+is_student = True #Boolean Datatype.
 print(is_student)
+
