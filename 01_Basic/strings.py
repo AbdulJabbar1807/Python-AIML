5af30e37009a3e0ab1df01fcc0bdcfeaffeeaf7e
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-04-16 09:10:27 +0530

    feat:string methods in python.

--- 01_Basic/strings.py
+++ 01_Basic/strings.py
@@ -5,3 +5,21 @@ last_name = "Khokhar"
 name = (first_name +" "+ middle_name + " " +last_name)
 print(name)
 print("-"*20)
+
+print(len(name))
+
+age = 21
+can_vote = age>=18 # Conditional operator.
+print(can_vote)
+if can_vote:
+    print("You can vote.")
+else:
+    print("You can't vote.")
+
+# String methods
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.capitalize())
+print(name.casefold())
+print(name.count("a"))
